fec36a8161ed03b2a4a0db89919cc56be3142ebc
YeEun <ydkrkek@gmail.com>
2023-09-02 17:23:48 +0900

    Add boj_2606_바이러스 Solved 1시간, DFS

---
+++ baekjoonAlgo/boj_2606_바이러스.cpp
@@ -0,0 +1,40 @@
+// 바이러스
+
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 101;
+int com[MAX][MAX]{};
+bool isVisit[MAX]{};
+int cnt{};
+
+void DFS_2606(int c, int N) {
+	if (cnt == N) return;
+
+	isVisit[c] = true;
+
+	for (int i = 1; i <= N; i++) {
+		if (isVisit[i] || com[c][i] == 0) continue;
+
+		cnt++;
+		DFS_2606(i, N);
+	}
+
+}
+
+int main() {
+	int N{}, M{}; // 컴퓨터 수, 직접 연결된 컴퓨터 쌍의 수
+	cin >> N >> M;
+
+	for (int i = 0; i < M; i++) {
+		int com1{}, com2{};
+		cin >> com1 >> com2;
+
+		com[com1][com2] = 1;
+		com[com2][com1] = 1;
+	}
+
+	DFS_2606(1, N);
+	cout << cnt;
+}
