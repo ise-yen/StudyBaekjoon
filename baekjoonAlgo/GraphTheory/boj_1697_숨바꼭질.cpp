0aabfde5be23f8e3dda1bf1e2546eed73c027ce7
YeEun <ydkrkek@gmail.com>
2023-09-08 09:28:31 +0900

    Add boj_1697_숨바꼭질 1시간30분, BFS 1차원 배열. 탐색 과정에서 횟수 최소값으로 check 필요. N==K 찾자마자 return 필요

---
+++ baekjoonAlgo/GraphTheory/boj_1697_숨바꼭질.cpp
@@ -0,0 +1,63 @@
+﻿// 숨바꼭질
+
+#include <iostream>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+bool isFind = false;
+
+const int MAX = 100001;
+bool pos[MAX]{};
+int check[MAX]{};
+
+int nextPos(int i, int N) {
+	if (i == 0) return N + 1;
+	if (i == 1) return N - 1;
+	if (i == 2) return N*2;
+}
+
+void BFS(int N, int K) {
+	queue<int> q;
+	q.push(N);
+
+	while (!q.empty()) {
+		for (int i = 0; i <= 20; i++) {
+			cout << check[i] << " ";
+		}
+
+		cout << endl;
+
+		N = q.front();
+		pos[N] = true;
+		q.pop();
+		if (N == K) {
+			cout << check[N];
+			return;
+		}
+
+		for (int i = 0; i < 3; i++) {
+			int nextN = nextPos(i, N);
+			if (nextN < 0 || nextN >= MAX || pos[nextN]) continue;
+
+			if (nextN == K) {
+				cout << check[N] + 1;
+				return;
+			}
+			q.push(nextN);
+			if(check[nextN]==0)
+				check[nextN] = check[N] + 1;
+			else {
+				check[nextN] = check[nextN] < check[N] + 1 ? check[nextN] : check[N] + 1;
+			}
+		}
+	}
+}
+
+int main() {
+	int N{}, K{};
+	cin >> N >> K;
+
+	BFS(N, K);
+}
