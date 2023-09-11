32032944620986e6e5ffb32da8858b191c75675f
YeEun <ydkrkek@gmail.com>
2023-09-11 15:27:17 +0900

    Add boj_10815_숫자카드 Solved 10분, unordered_map find(keyNum) != end()는 찾았다는 의미라는 점 주의하기

---
+++ baekjoonAlgo/DataStructure/boj_10815_숫자카드.cpp
@@ -0,0 +1,36 @@
+﻿// 숫자카드
+
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	
+	int N{}, M{};
+	unordered_map<int, int> ump;
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		int n{};
+		cin >> n;
+		if (ump.find(n) == ump.end()) {
+			ump.insert({ n, 1 });
+		}
+	}
+
+	cin >> M;
+	for (int i = 0; i < M; i++) {
+		int m{};
+		cin >> m;
+		if (ump.count(m)) {
+			cout << ump[m] << " ";
+		}
+		else {
+			cout << 0 << " ";
+		}
+	}
+}
