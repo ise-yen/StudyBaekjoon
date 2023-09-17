7a220c421b729604637f8508eaab43609658eb98
YeEun <ydkrkek@gmail.com>
2023-09-17 18:50:42 +0900

    Add boj_2839_설탕배달 Solved 1시간, Greedy

---
+++ baekjoonAlgo/Greedy/boj_2839_설탕배달.cpp
@@ -0,0 +1,29 @@
+﻿// 설탕배달
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int N{};
+	cin >> N;
+
+	int min = 5000;
+
+	if (N % 5 == 0) min = N / 5;
+	else {
+		for (int f = N / 5; f >= 0; f--) {
+			int n = N;
+			int sum = f;
+			n -= f * 5;
+			if (n % 3 == 0) {
+				sum += n / 3;
+				min = min < sum ? min : sum;
+			}
+			else continue;
+		}
+	}
+
+	if (min == 5000 ) cout << -1;
+	else cout << min;
+}
