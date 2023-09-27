0faf847247e98a7577d58f0caa61a2eca9f12875
YeEun <ydkrkek@gmail.com>
2023-09-27 17:20:54 +0900

    [Add] boj_11047_동전0 Solved 10분, 그리디: 나눗셈 반복

---
+++ baekjoonAlgo/Greedy/boj_11047_동전0.cpp
@@ -0,0 +1,29 @@
+﻿// 동전 0
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int N{}, K{};
+	cin >> N >> K;
+
+	vector<int> v;
+	for (int i = 0; i < N; i++) {
+		int a;
+		cin >> a;
+		v.push_back(a);
+	}
+
+	int cnt{};
+	for (int i = N - 1; i >= 0; i--) {
+		if (v[i] > K) continue;
+
+		cnt += K / v[i];
+		K %= v[i];
+		if (K == 0) break;
+	}
+
+	cout << cnt;
+}
