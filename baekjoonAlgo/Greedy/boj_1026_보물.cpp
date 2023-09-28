0462916002cd02fa9d7098feba2f9d20ca2df876
YeEun <ydkrkek@gmail.com>
2023-09-28 16:37:51 +0900

    [Add] boj_1026_보물 Solved 1분, sort

---
+++ baekjoonAlgo/Greedy/boj_1026_보물.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+    int N{};
+    vector<int> va;
+    vector<int> vb;
+
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        int a;
+        cin >> a;
+        va.push_back(a);
+    }
+    for (int i = 0; i < N; i++) {
+        int b;
+        cin >> b;
+        vb.push_back(b);
+    }
+
+    sort(va.begin(), va.end());
+    sort(vb.begin(), vb.end());
+
+    int res = 0;
+    for (int i = 0; i < N; i++) {
+        res += va[i] * vb[N - i - 1];
+    }
+    cout << res;
+}
