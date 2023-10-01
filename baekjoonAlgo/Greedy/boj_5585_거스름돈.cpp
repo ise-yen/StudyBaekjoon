6bbf9eaa53ab9b4cfbedf0b9550bf0d21b3fe816
YeEun <ydkrkek@gmail.com>
2023-10-01 22:14:59 +0900

    [Add] boj_5585_거스름돈 Solved 5분, Greedy

---
+++ baekjoonAlgo/Greedy/boj_5585_거스름돈.cpp
@@ -0,0 +1,22 @@
+﻿// 거스름돈
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int unit[] = { 500, 100, 50, 10, 5, 1 };
+	int money = 1000;
+	int cost{};
+	cin >> cost;
+
+	int remain = money - cost;
+
+	int res{};
+	for (int i = 0; i < sizeof(unit)/sizeof(int); i++) {
+		res += remain / unit[i];
+		remain %= unit[i];
+	}
+
+	cout << res;
+}
