60e8b4a39366b7a547638b4a84fb8774b705ff64
YeEun <ydkrkek@gmail.com>
2023-10-02 19:34:42 +0900

    [Add] boj_2217_로프 Solved 40분, Greedy, 구현

---
+++ baekjoonAlgo/Greedy/boj_2217_로프.cpp
@@ -0,0 +1,31 @@
+﻿// 로프
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int N{}, w{};
+	cin >> N;
+
+	vector<int> v;
+	for (int i = 0; i < N; i++) {
+		int rope{};
+		cin >> rope;
+		v.push_back(rope);
+	}
+	
+	sort(v.begin(), v.end());
+
+	int res = v[N - 1]; // 가장 무거운 로프 1개만 사용할 때의 중량
+	
+	// k개
+	for (int k = 2; k <= N; k++) {
+		int w = v[N - k] * k;
+		res = w > res ? w : res;
+	}
+
+	cout << res;
+}
