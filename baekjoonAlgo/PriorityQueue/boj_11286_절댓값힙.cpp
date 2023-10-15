e55acb797af1f579598c90a8f10b15849278b8a7
YeEun <ydkrkek@gmail.com>
2023-10-15 20:11:31 +0900

    [Add] boj_11286_절댓값힙 Solved 20분, priority queue <pair, greater>

---
+++ baekjoonAlgo/PriorityQueue/boj_11286_절댓값힙.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+#include<cmath>
+#include<queue>
+
+
+using namespace std;
+
+int main() {
+	int N{};
+	cin >> N;
+
+	priority_queue < pair<int, int>, vector<pair<int, int>>, greater<pair<int, int> >> pq;
+
+	for (int n = 0; n < N; n++) {
+		int x{};
+		cin >> x;
+
+		if (x == 0) {
+			if (pq.empty()) cout << "0\n";
+			else {
+				cout << pq.top().second << "\n";
+				pq.pop();
+			}
+		}
+		else {
+			pq.push({ abs(x), x });
+		}
+	}
+}
