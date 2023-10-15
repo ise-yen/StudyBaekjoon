893a310912619bfb61d0a63b5e3e8cbf2964eee3
YeEun <ydkrkek@gmail.com>
2023-10-15 20:10:45 +0900

    [Add] boj_11279_최대힙 Solved 10분, priority queue, c++ 시간 감소용 코드 추가 필수

---
+++ baekjoonAlgo/PriorityQueue/boj_11279_최대힙.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+#include<queue>
+
+using namespace std;
+
+int main() {
+	int N{};
+	cin >> N;
+
+	priority_queue<int> pq;
+	for (int n = 0; n < N; n++) {
+		int x{};
+		cin >> x;
+
+		if (x == 0) {
+			if (pq.empty()) cout << "0\n";
+			else {
+				cout << pq.top() << " ";
+				pq.pop();
+			}
+		}
+		else {
+			pq.push(x);
+		}
+
+	}
+}
